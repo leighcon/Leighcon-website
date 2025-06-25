4e729120d53f1db55d80b1b4993477cf8293557d
leighcon <josephthomasau@gmail.com>
2025-06-25 15:11:48 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  root: "client",
+  root: "./client/src/client/src/client",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client/src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
+      "@": "/src",
+      "@shared": "../../../../shared",
+      "@assets": "../../../../attached_assets",
     },
   },
   build: {
-    outDir: "../dist/public",
+    outDir: "../../../../dist/public",
     emptyOutDir: true,
   },
 });
