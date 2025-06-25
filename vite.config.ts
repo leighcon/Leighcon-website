be0ab3c5796978c3dbc83f1589f3934bd83144ad
leighcon <josephthomasau@gmail.com>
2025-06-25 13:05:27 +1000

    Fix client vite config for ES modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(process.cwd(), "client/src"),
-      "@shared": path.resolve(process.cwd(), "shared"),
-      "@assets": path.resolve(process.cwd(), "attached_assets"),
+      "@": path.resolve(__dirname, "src"),
+      "@shared": path.resolve(__dirname, "../shared"),
+      "@assets": path.resolve(__dirname, "../attached_assets"),
     },
   },
   build: {
-    rollupOptions: {
-      input: path.resolve(process.cwd(), "client/index.html")
-    },
-    outDir: "dist/public",
+    outDir: "../dist/public",
     emptyOutDir: true,
   },
 });
