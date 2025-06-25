01a09d33aceeaadcedaf8ed612c0d576b2f01416
leighcon <josephthomasau@gmail.com>
2025-06-25 15:15:49 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
     },
   },
   build: {
+    rollupOptions: {
+      input: "./index.html"
+    },
     outDir: "../../../dist/public",
     emptyOutDir: true,
   },
